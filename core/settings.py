6bb722166cf54359cfca9ea31cb8a76b401cce5a
Slava Golyshevskii <viacheslav.golyshevskiy@gamil.com>
2022-11-15 15:21:42 +0400

    main app config

--- core/settings.py
+++ core/settings.py
@@ -24,6 +24,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # app
+    'main',
+
+    # lb
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
