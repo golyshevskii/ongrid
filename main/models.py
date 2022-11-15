578e187e71cc0cd459c76cdf79a601af009d6600
Slava Golyshevskii <viacheslav.golyshevskiy@gamil.com>
2022-11-15 15:50:31 +0400

    model + manager

--- main/models.py
+++ main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class CustomeManager(BaseUserManager):
+    """custome manager for creating user and superuser"""
+
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('User must have an email')
+        
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password=password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password=password)
+        user.is_admin = True
+        user.save()
+        return user
+
+
+class CustomeUser(AbstractUser):
+    """custome user model with email main username field"""
+
+    email = models.CharField(max_length=255, unique=True, verbose_name='Email')
+    name = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomeManager()
+
+    def __str__(self):
+        return self.email
