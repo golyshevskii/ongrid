b94bd948c6e4a6e7dad383713363991ed69768fb
Slava Golyshevskii <viacheslav.golyshevskiy@gamil.com>
2022-11-15 21:21:53 +0400

    login config

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views
-from .views import main_page
+from .views import main_page, login_page
 
 
 urlpatterns = [
     path('', main_page, name='mainpage'),
-    path('login/', views.LoginView.as_view(template_name='base.html'), name='login'),
+    path('login/', login_page, name='login'),
     path('logout/', views.LogoutView.as_view(template_name='base.html'), name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
-# Create your views here.
+
+@login_required(login_url='/login/')
 def main_page(request):
     return render(request, 'base.html')
+
+
+def login_page(request):
+    context = {'val_err': None, 'emp_pass': None}
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        try:
+            validate_email(email)
+        except ValidationError:
+            context['val_err'] = 'Email address is not valid'
+
+        if password:
+            user = authenticate(request, email=email, password=password)
+            login(request, user)
+            return redirect('main:mainpage')
+        else:
+            context['emp_pass'] = 'Password required'
+
+    return render(request, 'login.html', context=context)
