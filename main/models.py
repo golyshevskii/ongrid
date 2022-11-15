f93350f276c504acce993b52cc46e3c8a8713b25
Slava Golyshevskii <viacheslav.golyshevskiy@gamil.com>
2022-11-15 16:23:17 +0400

    email auth

--- main/models.py
+++ main/models.py
@@ -3,30 +3,32 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class CustomeManager(BaseUserManager):
-    """custome manager for creating user and superuser"""
+    """Custome manager for creating user and superuser"""
 
     def create_user(self, email, password=None):
         if not email:
             raise ValueError('User must have an email')
         
         user = self.model(email=self.normalize_email(email))
-        user.set_password(password=password)
+        user.set_password(password)
         user.save()
 
         return user
 
     def create_superuser(self, email, password=None):
         user = self.create_user(email, password=password)
-        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
         user.save()
         return user
 
 
 class CustomeUser(AbstractUser):
-    """custome user model with email main username field"""
+    """Custome user model with email main username field"""
 
-    email = models.CharField(max_length=255, unique=True, verbose_name='Email')
     name = None
+    email = models.CharField(max_length=255, unique=True, verbose_name='Email')
+    is_active = models.BooleanField(default=True, verbose_name='Is Active')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
