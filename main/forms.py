410002237a844a53b755e4ea82db2d7fa2fcebf6
Slava Golyshevskii <viacheslav.golyshevskiy@gamil.com>
2022-11-16 13:19:41 +0400

    login form

--- main/forms.py
+++ main/forms.py
@@ -3,6 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomeUser
 
 
+class LoginForm(forms.Form):
+    """Form for login user"""
+
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
 class SignUpForm(UserCreationForm):
     """Form for new user registration"""
 
